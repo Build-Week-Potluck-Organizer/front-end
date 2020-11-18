6c8ed1bcdb1eccbd17624b64b438e1f45cddffb0
Heather Beckman <heatherbeckman85@gmail.com>
2020-11-18 08:43:19 -0700

    added some styling

--- PotluckOrganizer/src/components/Homepage.js
+++ PotluckOrganizer/src/components/Homepage.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 const Break = styled.hr`
     width: 75%;
     margin-top: 30px;
+`;
+
+const Header3 = styled.h3`
+    margin-right: 60%;
+`;
+
+const Flex = styled.div`
+    width: 60%;
+    display: flex;
+    justify-content: space-evenly;
+    margin-left: 15%;
 `;
 
 export const Homepage = () => {
@@ -11,14 +23,20 @@ export const Homepage = () => {
         <div>
             <h2>Welcome, username!</h2>
             <button>Create an event</button>
-            <h3>Your Events</h3>
-            <span>Event name </span><span>date</span>
-            <button>edit</button>
-            <button>delete</button>
+            <Header3>Your Events</Header3>
+            <Flex>
+                <span>Raine's Amazing Birthday Party </span><span>date</span>
+                <div>
+                    <button>edit</button>
+                    <button>delete</button>
+                </div>
+            </Flex>
             <Break></Break>
-            <h3>Invites</h3>
-            <span>Event name </span><span>date</span>
-            <button>RSVP</button>
+            <Header3>Invitations</Header3>
+            <Flex>
+                <span>Event name </span><span>date</span>
+                <button>RSVP</button>
+            </Flex>
         </div>
     )
 };
