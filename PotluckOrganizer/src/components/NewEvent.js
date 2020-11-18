e12aa2660860a6c5a2f4fee58592f9a69b29a788
Heather Beckman <heatherbeckman85@gmail.com>
2020-11-18 10:39:02 -0700

    working on newevent component

--- PotluckOrganizer/src/components/NewEvent.js
+++ PotluckOrganizer/src/components/NewEvent.js
@@ -2,19 +2,33 @@ import React, {useState} from 'react';
 
 export const NewEvent = () => {
     const [form, setForm] = useState({
-        event_id: 1,
-        event_name: "",
-        description: "",
-        organizer_id: 1,
-        date: "",
-        time: ""
-    });
+        
+        });
+
 
     return (
         <div>
             <h2>Create a new event:</h2>
             <form>
-               
+                <label>
+                    Event Name:
+                    <input></input>
+                </label>
+                <br></br>
+                <label>
+                    Description:
+                    <input type='textbox'></input>
+                </label>
+                <br></br>
+                <label>
+                    Date:
+                    <input></input>
+                </label>
+                <br></br>
+                <label>
+                    Time:
+                    <input></input>
+                </label>
             </form>
         </div>
     )
